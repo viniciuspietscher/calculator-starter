36bb1f29fa2324ac042814126fe27f3d16292343
Vinicius Pietscher <8095975+viniciuspietscher@users.noreply.github.com>
2022-11-04 14:54:02 -0400

    update api tests, update api

--- __tests__/utils/api/api.test.js
+++ __tests__/utils/api/api.test.js
@@ -30,14 +30,25 @@ describe("Wrong number of params", () => {
     handler(req, res)
     expect(res._status).toBe(500)
     expect(res._json).toEqual({
-      message: `Query params should have 3 items. Received 4: add,2,2,2`,
+      message: `Query params should have 3 items. Received ${req.query.params.length}: ${req.query.params}`,
+    })
+  })
+
+  it("throws an error if user submits less than 3 params", () => {
+    const req = getRequestObject("GET", [2, 2])
+    const res = getResponseObject()
+
+    handler(req, res)
+    expect(res._status).toBe(500)
+    expect(res._json).toEqual({
+      message: `Query params should have 3 items. Received ${req.query.params.length}: ${req.query.params}`,
     })
   })
 })
 
 //
 describe("Calculate API tests", () => {
-  it("returns a valid result when getting valid params", () => {
+  it("returns a valid result when Adding integers", () => {
     const req = getRequestObject("GET", ["add", 2, 2])
     const res = getResponseObject()
 
@@ -45,9 +56,93 @@ describe("Calculate API tests", () => {
     expect(res._status).toBe(200)
     expect(res._json).toEqual({ result: 4 })
   })
+
+  it("returns a valid result when Adding float numbers", () => {
+    const req = getRequestObject("GET", ["add", 2.2, 2.2])
+    const res = getResponseObject()
+
+    handler(req, res)
+    expect(res._status).toBe(200)
+    expect(res._json).toEqual({ result: 4.4 })
+  })
+
+  it("returns a valid result when Subtracting integers", () => {
+    const req = getRequestObject("GET", ["subtract", 2, 2])
+    const res = getResponseObject()
+
+    handler(req, res)
+    expect(res._status).toBe(200)
+    expect(res._json).toEqual({ result: 0 })
+  })
+
+  it("returns a valid result when Subtracting float numbers", () => {
+    const req = getRequestObject("GET", ["subtract", 2.2, 2.2])
+    const res = getResponseObject()
+
+    handler(req, res)
+    expect(res._status).toBe(200)
+    expect(res._json).toEqual({ result: 0 })
+  })
+
+  it("returns a valid result when Multiplying integers", () => {
+    const req = getRequestObject("GET", ["multiply", 2, 3])
+    const res = getResponseObject()
+
+    handler(req, res)
+    expect(res._status).toBe(200)
+    expect(res._json).toEqual({ result: 6 })
+  })
+
+  it("returns a valid result when Multiplying float numbers", () => {
+    const req = getRequestObject("GET", ["multiply", 3.5, 2.9])
+    const res = getResponseObject()
+
+    handler(req, res)
+    expect(res._status).toBe(200)
+    expect(res._json).toEqual({ result: 3.5 * 2.9 })
+  })
+
+  it("returns a valid result when Dividing integers", () => {
+    const req = getRequestObject("GET", ["divide", 10, 2])
+    const res = getResponseObject()
+
+    handler(req, res)
+    expect(res._status).toBe(200)
+    expect(res._json).toEqual({ result: 5 })
+  })
+
+  it("returns a valid result when Dividing float numbers", () => {
+    const req = getRequestObject("GET", ["divide", 20.2, 1.5])
+    const res = getResponseObject()
+
+    handler(req, res)
+    expect(res._status).toBe(200)
+    expect(res._json).toEqual({ result: 20.2 / 1.5 })
+  })
+
+  it("throws an error when Dividing by zero", () => {
+    const req = getRequestObject("GET", ["divide", 20, 0])
+    const res = getResponseObject()
+
+    handler(req, res)
+    expect(res._status).toBe(500)
+    expect(res._json).toEqual({ message: "Can't divide by zero" })
+  })
 })
 
-//
+describe("Unsupported params", () => {
+  it("throws an error if user submits a String instead of a number", () => {
+    const req = getRequestObject("GET", ["add", "a", 2])
+    const res = getResponseObject()
+
+    handler(req, res)
+    expect(res._status).toBe(500)
+    expect(res._json).toEqual({
+      message: `Failed to process query params. Received: ${req.query.params}`,
+    })
+  })
+})
+
 describe("Unsupported methods:", () => {
   it("throws an error if user sends a POST request", () => {
     const req = getRequestObject("POST", ["add", 2, 2])
--- pages/api/calculate/[...params].js
+++ pages/api/calculate/[...params].js
@@ -1,34 +1,34 @@
-import { add, subtract, multiply, divide } from "../../../utils/calculate";
+import { add, subtract, multiply, divide } from "../../../utils/calculate"
 
 export default function handler(req, res) {
   try {
     if (req.method !== "GET") {
       throw new Error(
         `Unsupported method ${req.method}. Only GET method is supported`
-      );
+      )
     }
 
-    const params = extractParams(req.query.params);
-    let result;
+    const params = extractParams(req.query.params)
+    let result
     switch (params.operation) {
       case "add":
-        result = add(params.first, params.second);
-        break;
+        result = add(params.first, params.second)
+        break
       case "subtract":
-        result = subtract(params.first, params.second);
-        break;
+        result = subtract(params.first, params.second)
+        break
       case "multiply":
-        result = multiply(params.first, params.second);
-        break;
+        result = multiply(params.first, params.second)
+        break
       case "divide":
-        result = divide(params.first, params.second);
-        break;
+        result = divide(params.first, params.second)
+        break
       default:
-        throw new Error(`Unsupported operation ${params.operation}`);
+        throw new Error(`Unsupported operation ${params.operation}`)
     }
-    res.status(200).json({ result });
+    res.status(200).json({ result })
   } catch (e) {
-    res.status(500).json({ message: e.message });
+    res.status(500).json({ message: e.message })
   }
 }
 
@@ -36,18 +36,17 @@ function extractParams(queryParams) {
   if (queryParams.length !== 3) {
     throw new Error(
       `Query params should have 3 items. Received ${queryParams.length}: ${queryParams}`
-    );
+    )
   }
 
-  try {
-    const params = {
-      operation: queryParams[0],
-      first: parseInt(queryParams[1]),
-      second: parseInt(queryParams[2]),
-    };
-    return params;
-  } catch (e) {
-    throw new Error(`Failed to process query params. Received: ${queryParams}`);
+  const params = {
+    operation: queryParams[0],
+    first: Number(queryParams[1]),
+    second: Number(queryParams[2]),
   }
+  if (isNaN(params.first) || isNaN(params.second)) {
+    throw new Error(`Failed to process query params. Received: ${queryParams}`)
+  }
+
+  return params
 }
-
