d20ff1b1730778256e273dea4d3b0fd242f937d6
Vinicius Pietscher <8095975+viniciuspietscher@users.noreply.github.com>
2022-11-04 20:21:33 -0400

    add more operation tests

--- e2e/calculator.spec.js
+++ e2e/calculator.spec.js
@@ -1,13 +1,112 @@
 import { test, expect } from "@playwright/test"
 
-test("should test add operation", async ({ page }) => {
-  await page.goto("/")
-  await page.type("#first", "2")
-  await page.type("#second", "3")
-  await page.click("#operation")
-  await page.locator("#operation").selectOption("add")
-  await page.click('button[type="submit"]')
+test.describe("Operations values with valid inputs", () => {
+  test("Integer Add operation", async ({ page }) => {
+    await page.goto("/")
+    await page.type("#first", "2")
+    await page.type("#second", "3")
+    await page.click("#operation")
+    await page.locator("#operation").selectOption("add")
+    await page.click('button[type="submit"]')
 
-  const result = page.locator("#result")
-  await expect(result).toContainText("5")
+    const result = page.locator("#result")
+    await expect(result).toContainText("5")
+  })
+  test("Integer Subtract operation", async ({ page }) => {
+    await page.goto("/")
+    await page.type("#first", "2")
+    await page.type("#second", "3")
+    await page.click("#operation")
+    await page.locator("#operation").selectOption("subtract")
+    await page.click('button[type="submit"]')
+
+    const result = page.locator("#result")
+    await expect(result).toContainText("1")
+  })
+  test("Integer Multiply operation", async ({ page }) => {
+    await page.goto("/")
+    await page.type("#first", "2")
+    await page.type("#second", "3")
+    await page.click("#operation")
+    await page.locator("#operation").selectOption("multiply")
+    await page.click('button[type="submit"]')
+
+    const result = page.locator("#result")
+    await expect(result).toContainText("6")
+  })
+  test("Integer Divide operation", async ({ page }) => {
+    await page.goto("/")
+    await page.type("#first", "2")
+    await page.type("#second", "3")
+    await page.click("#operation")
+    await page.locator("#operation").selectOption("divide")
+    await page.click('button[type="submit"]')
+
+    const result = page.locator("#result")
+    await expect(result).toContainText(`${2 / 3}`)
+  })
+
+  test("Float number Add operation", async ({ page }) => {
+    await page.goto("/")
+    await page.type("#first", "2.5")
+    await page.type("#second", "3.2")
+    await page.click("#operation")
+    await page.locator("#operation").selectOption("add")
+    await page.click('button[type="submit"]')
+
+    const result = page.locator("#result")
+    await expect(result).toContainText(`${2.5 + 3.2}`)
+  })
+  test("Float number Subtract operation", async ({ page }) => {
+    await page.goto("/")
+    await page.type("#first", "2.5")
+    await page.type("#second", "3.2")
+    await page.click("#operation")
+    await page.locator("#operation").selectOption("subtract")
+    await page.click('button[type="submit"]')
+
+    const result = page.locator("#result")
+    await expect(result).toContainText(`${2.5 - 3.2}`)
+  })
+  test("Float number Multiply operation", async ({ page }) => {
+    await page.goto("/")
+    await page.type("#first", "2.5")
+    await page.type("#second", "3.2")
+    await page.click("#operation")
+    await page.locator("#operation").selectOption("multiply")
+    await page.click('button[type="submit"]')
+
+    const result = page.locator("#result")
+    await expect(result).toContainText(`${2.5 * 3.2}`)
+  })
+  test("Float number Divide operation", async ({ page }) => {
+    await page.goto("/")
+    await page.type("#first", "2.5")
+    await page.type("#second", "3.2")
+    await page.click("#operation")
+    await page.locator("#operation").selectOption("divide")
+    await page.click('button[type="submit"]')
+
+    const result = page.locator("#result")
+    await expect(result).toContainText(`${2.5 / 3.2}`)
+  })
 })
+
+// test("Invalid input error messages:", async ({ page }) => {
+//   const reqObject = {
+//     operation: "add",
+//     first: "a",
+//     second: 3,
+//   }
+//   await page.goto("/")
+//   await page.type("#first", reqObject.first)
+//   await page.type("#second", reqObject.second)
+//   await page.click("#operation")
+//   await page.locator("#operation").selectOption(reqObject.operation)
+//   await page.click('button[type="submit"]')
+
+//   const result = page.locator("#result")
+//   await expect(result).toContainText(
+//     `Failed to process query params. Received: ${reqObject.operation} ${reqObject.first} ${reqObject.second}`
+//   )
+// })
--- playwright.config.js
+++ playwright.config.js
@@ -33,8 +33,10 @@ const config = {
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
+
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  // reporter: "html",
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
@@ -56,12 +58,12 @@ const config = {
 
   /* Configure projects for major browsers */
   projects: [
-    {
-      name: "chromium",
-      use: {
-        ...devices["Desktop Chrome"],
-      },
-    },
+    // {
+    //   name: "chromium",
+    //   use: {
+    //     ...devices["Desktop Chrome"],
+    //   },
+    // },
 
     {
       name: "firefox",
